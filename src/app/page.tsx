4a099abf4472ead69097bbb76febc067604ec8a6
aramind <88814622+aramind@users.noreply.github.com>
2025-08-12 23:26:16 +0800

    feat: adding more feature cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,6 +107,38 @@ export default function Home() {
                   </div>
                 </CardContent>
               </Card>
+              <Card>
+                <CardContent className="pt-6">
+                  <div className="flex flex-col justify-center space-y-4">
+                    <Paperclip className="w-10 h-10 text-black" />
+                    <div className="space-y-2">
+                      <h3 className="text-xl font-bold">
+                        Easy Request Submission
+                      </h3>
+                      <p className="text-gray-500 dark:text-gray-400">
+                        Employees can submit time off requests directly through
+                        the platform.
+                      </p>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardContent className="pt-6">
+                  <div className="flex flex-col justify-center space-y-4">
+                    <Paperclip className="w-10 h-10 text-black" />
+                    <div className="space-y-2">
+                      <h3 className="text-xl font-bold">
+                        Easy Request Submission
+                      </h3>
+                      <p className="text-gray-500 dark:text-gray-400">
+                        Employees can submit time off requests directly through
+                        the platform.
+                      </p>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
           </div>
         </section>
