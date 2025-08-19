afdceeaae26dfacdf5a68d93dcd5a44e1a1f6f67
aramind <88814622+aramind@users.noreply.github.com>
2025-08-19 16:46:20 +0800

    feat: setting up the  middle wares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,59 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isPublicRoute(req)) return NextResponse.next();
-});
+import { NextRequest, NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/api/webhooks/(.*)"]);
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId, sessionClaims, redirectToSignIn } = await auth();
+  const isOnboardingRoute = createRouteMatcher("/onboarding");
+  const isAdminRoute = createRouteMatcher(["/admin", "/admin/(.*)"]);
+  const isEmployeeRoute = createRouteMatcher(["/employee", "/employee/(.*)"]);
+
+  console.log(sessionClaims?.metadata);
+
+  if (isPublicRoute(req)) return NextResponse.next();
+
+  if (!userId && !isPublicRoute) {
+    return redirectToSignIn({
+      returnBackUrl: req.url,
+    });
+  }
+
+  if (userId && isOnboardingRoute(req)) {
+    return NextResponse.next();
+  }
+
+  if (
+    userId &&
+    !sessionClaims?.metadata?.onboardingCompleted &&
+    !isOnboardingRoute(req)
+  ) {
+    const onboardingUrl = new URL("/onboarding", req.url);
+    return NextResponse.redirect(onboardingUrl);
+  }
+
+  if (isAdminRoute(req)) {
+    if (sessionClaims?.metadata?.role === "ADMIN") {
+      return NextResponse.next();
+    } else {
+      const homepageUrl = new URL("/", req.url);
+      return NextResponse.redirect(homepageUrl);
+    }
+  }
+
+  if (isEmployeeRoute(req)) {
+    if (sessionClaims?.metadata?.role === "EMPLOYEE") {
+      return NextResponse.next();
+    } else {
+      const homepageUrl = new URL("/", req.url);
+      return NextResponse.redirect(homepageUrl);
+    }
+  }
+
+  if (userId) {
+    return NextResponse.next();
+  }
+});
 
 export const config = {
   matcher: [
