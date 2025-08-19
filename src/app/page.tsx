a594fec15370ba0068027f21a6386f1f3e8ba970
aramind <88814622+aramind@users.noreply.github.com>
2025-08-19 14:14:05 +0800

    feat: setting up clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { Paperclip } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,11 +32,24 @@ export default function Home() {
             How it works
           </Link>
           <Link
-            href="/features"
+            href="/pricing"
             className="text-sm font-medium hover:underline underline-offset-4"
           >
-            Features
+            Pricing
           </Link>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         </nav>
       </header>
       <main className="flex-1">
